3d35dfdebb00176618f379bde1c3e4f2798988f6
Versatile-Programmer <rajraushan7112002@gmail.com>
2025-02-19 01:45:36 +0530

    do some changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-
+const app = express();
 app.use(express.json());
 const port = 5000;
 app.get('/', (req, res) => {
     res.send('Trying CI/CD pipeline');
 })
-
+//Adding a comments to make some difference in file.
 app.listen(port, () => {
     console.log(`🚀 Server is running on http://localhost:${port}`);
   });
